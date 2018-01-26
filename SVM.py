1698c4eb5958ca0146677d4bc2b42f3dece57faa
Sheshansh Agrawal <sheshansh@cse.iitb.ac.in>
2018-01-26 18:12:26 +0530

    Checkpoint 1, simple convex SVM done

--- SVM.py
+++ SVM.py
@@ -4,9 +4,9 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 import tensorflow as tf
 import numpy as np
 
-posdigit = 9
-negdigit = 6
-labelled_unlabelled_split_barrier = 20
+posdigit = 8
+negdigit = 3
+labelled_unlabelled_split_barrier = 2000
 
 theta_p = 0.5
 theta_n = 1-theta_p
@@ -86,15 +86,16 @@ hinge_loss_NU = lossplusg(logits_unlabelled)-theta_n*lossplusg(logits_neg)+theta
 # loss_op = hinge_loss_PU + regularization_loss
 # loss_op = hinge_loss_NU + regularization_loss
 
-# def run(gamma):
-# 	loss_op = hinge_loss_NU*gamma + hinge_loss_PU * (1-gamma) + regularization_loss
-# 	loss_op = hinge_loss_PN + regularization_loss
-def run(eta):
+def run(gamma):
+	loss_op = hinge_loss_NU*gamma + hinge_loss_PU * (1-gamma) + regularization_loss
+	# loss_op = hinge_loss_PN + regularization_loss
 
-	if eta >=0 :
-		loss_op = hinge_loss_PU*eta+hinge_loss_PN*(1-eta)+regularization_loss
-	else:
-		loss_op = hinge_loss_NU*eta+hinge_loss_PN*(1-eta)+regularization_loss
+# def run(eta):
+
+# 	if eta >=0 :
+# 		loss_op = hinge_loss_PU*eta+hinge_loss_PN*(1-eta)+regularization_loss
+# 	else:
+# 		loss_op = hinge_loss_NU*eta+hinge_loss_PN*(1-eta)+regularization_loss
 
 	train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_op)
 	# train_op = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss_op)
@@ -116,15 +117,6 @@ def run(eta):
 
 		for step in range(1, num_steps+1):
 			# Run optimization op (backprop)
-			# sess.run(train_op, feed_dict={X_pos: pos_x,
-			# 								X_neg: neg_x
-			# 							 })
-			# sess.run(train_op, feed_dict={X_pos: pos_x,
-			# 								X_unlabelled: unlabelled_data_x
-			# 							 })
-			# sess.run(train_op, feed_dict={X_neg: neg_x,
-			# 								X_unlabelled: unlabelled_data_x
-			# 							 })
 			sess.run(train_op, feed_dict={X_pos: pos_x,
 											X_neg: neg_x,
 											X_unlabelled: unlabelled_data_x
@@ -152,29 +144,33 @@ def run(eta):
 																		 X: np.concatenate((pos_x,neg_x)),
 																		 Y: np.concatenate((np.array([1.0]*pos_x.shape[0]),np.array([-1.0]*neg_x.shape[0]))).reshape(-1,1),
 																	 })
-				print("Step " + str(step) + ", Minibatch Loss= " + \
-					  "{:.4f}".format(loss) + ", Training Accuracy= " + \
-					  "{:.3f}".format(acc))
+				# print("Step " + str(step) + ", Minibatch Loss= " + \
+				# 	  "{:.4f}".format(loss) + ", Training Accuracy= " + \
+				# 	  "{:.3f}".format(acc))
 
-		print("Optimization Finished!")
+		# print("Optimization Finished!")
 		# Calculate accuracy for MNIST test images
 		# print("Testing Accuracy:", \
 		# 	list(sess.run(predicted_class, feed_dict={X: test_data_x})))
-		print("Testing Accuracy:", \
+		print(gamma,",", \
 			sess.run(accuracy, feed_dict={X: test_pos,
-										  Y: np.array([1.0]*test_pos.shape[0]).reshape(-1,1)}))
-		print("Testing Accuracy:", \
+										  Y: np.array([1.0]*test_pos.shape[0]).reshape(-1,1)})
+			, ",",
 			sess.run(accuracy, feed_dict={X: test_neg,
-										  Y: np.array([-1.0]*test_neg.shape[0]).reshape(-1,1)}))
-		print("Testing Accuracy:", \
+										  Y: np.array([-1.0]*test_neg.shape[0]).reshape(-1,1)})
+			, ",",
 			sess.run(accuracy, feed_dict={X: test_data_x,
-										  Y: test_labels}))
+										  Y: test_labels})
+			)
+		# print("Testing Accuracy:", \
+		# 	)
+		# print("Testing Accuracy:", \
+		# 	)
 		save_path = tf.train.Saver([v for v in tf.global_variables()]).save(sess, 'models/model_pnu.ckpt')
-		print("Model saved in file: %s" % save_path)
+		# print("Model saved in file: %s" % save_path)
 
 
-eta = -0.09
-while eta <= 0.05:
-	print(eta)
+eta = 0.05
+while eta <=1.0:
 	run(eta)
-	eta += 0.03
+	eta += 0.05
